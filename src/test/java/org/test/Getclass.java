3bd72304af8ad079898c75d66c989f93603dacaa
vinothkumar7305897608 <vinothboss1977@gmail.com>
2022-09-07 11:38:17 +0530

    kumar's code

--- src/test/java/org/test/Getclass.java
+++ src/test/java/org/test/Getclass.java
@@ -2,4 +2,28 @@ package org.test;
 
 public class Getclass {
 
+	
+	public static void main(String[] args) {
+	System.out.println("Test1");	
+	System.out.println("Test2");	
+	System.out.println("Test3");	
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
